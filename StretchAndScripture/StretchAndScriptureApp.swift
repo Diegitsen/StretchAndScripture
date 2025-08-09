090c6bd5ab7022a37d853f6d6e2d31fc30013c73
diego <diegitsen@gmail.com>
2025-08-09 14:41:33 +0200

    first commit

---
+++ StretchAndScripture/StretchAndScriptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchAndScriptureApp.swift
+//  StretchAndScripture
+//
+//  Created by diegitsen on 9/08/25.
+//
+
+import SwiftUI
+
+@main
+struct StretchAndScriptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StretchAndScriptureTests/StretchAndScriptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  StretchAndScriptureTests.swift
+//  StretchAndScriptureTests
+//
+//  Created by diegitsen on 9/08/25.
+//
+
+import Testing
+@testable import StretchAndScripture
+
+struct StretchAndScriptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
